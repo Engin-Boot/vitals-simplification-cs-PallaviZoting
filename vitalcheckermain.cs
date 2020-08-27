f787e27de5fe99de118d12d47a2d832f59034e63
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-27 21:32:39 +0530

    Create vitalcheckermain.cs

---
+++ vitalcheckermain.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics;
+using static Checker;
+
+class VitalCheckerMain
+{
+    static void ExpectTrue(bool expression) {
+        if(!expression) {
+            Console.WriteLine("Expected true, but got false");
+            Environment.Exit(1);
+        }
+    }
+    static void ExpectFalse(bool expression) {
+        if(expression) {
+            Console.WriteLine("Expected false, but got true");
+            Environment.Exit(1);
+        }
+    }
+    static int Main() {
+        ExpectTrue(vitalsAreOk(100, 95, 60));
+        ExpectFalse(vitalsAreOk(40, 91, 92));
+        Console.WriteLine("All ok");
+        return 0;
+    }
+}
