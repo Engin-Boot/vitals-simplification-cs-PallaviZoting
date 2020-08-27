ea94a2bd6ad9d479795f848a04d1efd1c90c9439
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-27 21:34:19 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -13,16 +13,6 @@ class Checker
         }
         return true;
     }
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
-        }
-    }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
-        }
-    }
+
+    
 }
