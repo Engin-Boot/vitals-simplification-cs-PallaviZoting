e1a62c1e7bc2cfd4479f8a3a99e31127fd0c1c70
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-08-28 11:23:26 +0530

    Update VitalChecker.cs

--- VitalChecker.cs
+++ VitalChecker.cs
@@ -3,15 +3,16 @@ using System.Diagnostics;
 
 namespace Vitals
 {
-class VitalChecker
-{
-   public static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-   if (RespRateChecker.respRateVitalsareOk(respRate) && Spo2Checker.spo2VitalsareOk(spo2) && BmpChecker.bmpVitalsareOk(bmp))
-    {
+   class VitalChecker
+   {
+      public static bool vitalsAreOk(float bpm, float spo2, float respRate) {
+      if (RespRateChecker.respRateVitalsareOk(respRate) && Spo2Checker.spo2VitalsareOk(spo2) && BmpChecker.bmpVitalsareOk(bmp))
+      {
         return true;
-    }
-    else{
-    return false;
-    }
-}
+      }
+      else{
+        return false;
+      }
+      }
+  }
 }
