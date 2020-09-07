dbc01c8d349769518d7c866e41dd354b167b9705
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-09-07 14:04:40 +0530

    Update VitalChecker.cs

--- VitalChecker.cs
+++ VitalChecker.cs
@@ -11,6 +11,10 @@ namespace Vitals
          bool vital2 = RangeChecker.rangeCheckerVitals(90,100,spo2);
          bool vital3 = RangeChecker.rangeCheckerVitals(70,150,bpm);
          bool vitalcheck = vital1 && vital2 && vital3;
+         AbnormalReading.abnormal_respRate(30,95,respRate);
+         AbnormalReading.abnormal_spo2(90,100,spo2);	
+         AbnormalReading.abnormal_bpm(70,150,bpm);
+         return vitalcheck;
          return vitalcheck;
       }
    }
