cad3184b493b2371d6df28c31856c47565d05912
PallaviZoting <68611958+PallaviZoting@users.noreply.github.com>
2020-09-07 11:38:39 +0530

    Update VitalChecker.cs

--- VitalChecker.cs
+++ VitalChecker.cs
@@ -2,14 +2,14 @@ using System;
 using System.Diagnostics;
 
 namespace Vitals
-{
-   class VitalChecker
+{ 
+ class VitalChecker
    {
       public static bool vitalsAreOk(float bpm, float spo2, float respRate)
       {
-         bool vital1 = RespRateChecker.respRatevitalsAreOk(respRate);
-         bool vital2 = Spo2Checker.spo2vitalsAreOk(spo2);
-         bool vital3 = BmpChecker.bmpVitalsAreOk(bpm);
+         bool vital1 = RangeChecker.rangeCheckerVitals(30,95,respRate);
+         bool vital2 = RangeChecker.rangeCheckerVitals(90,100,spo2);
+         bool vital3 = RangeChecker.rangeCheckerVitals(70,150,bpm);
          bool vitalcheck = vital1 && vital2 && vital3;
          return vitalcheck;
       }
